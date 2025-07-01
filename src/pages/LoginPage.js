621cf0d8f7f9d34eef5cd97821a05aa1e031c867
rumarsky <glebmlakir.one@gmail.com>
2025-07-01 10:50:27 +1000

    Интеграция эндпоинта авторизации

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -7,10 +7,47 @@ function LoginPage({ onLogin, switchToRegister }) {
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = e => {
+  function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+  async function loginUser({email, password}) {
+    const response = await fetch("http://81.163.28.17:10001/api/login", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({email, password})
+    })
+
+    //1. получить токены +
+    //2. сохранить токены в куки +
+    //3. передавать токены в запросы +
+    //4. если аксес истек - обновить - для этого во всех частях программы нужно будет делать проверку... и лучше ее будто вынести в отдельный компонент
+    //5. предыдущие затереть -
+    //6. если оба стерлись -
+
+    if (!response.ok){
+      const error = await response.text();
+      throw new Error(error || 'Ошибка регистрации');
+    } else {
+      return await response.json()
+    }
+  }
+
+  const handleSubmit = async e => {
     e.preventDefault();
     // Здесь логика авторизации
-    onLogin?.(email, password);
+    try {
+      const data = await loginUser({email, password})
+      document.cookie = `accessToken=${encodeURIComponent(data.accessToken)}`
+      document.cookie = `refreshToken=${encodeURIComponent(data.refreshToken)}`
+      //с помощью функции getCookie можно получить дсоутп к любой куке. Но будет ли это работать и в других частях программы?
+      navigate("/") 
+    } catch (err) {
+
+    }
   };
 
   return (
