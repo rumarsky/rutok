42329c0c1a6599a5b0b34993b3d16a0ca2402466
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-07-02 21:51:01 +1000

    add fetching user data

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar';
-import userService from '../services/userService';
-import './SettingsPage.css';
+import React, { useEffect, useState } from "react";
+import Sidebar from "../components/Sidebar";
+import userService from "../services/userService";
+import authService from "../services/authService";
+import { jwtDecode } from "jwt-decode";
+import "./SettingsPage.css";
 
 function SettingsPage() {
-  const [name, setName] = useState('Иван Иванов');
-  const [email, setEmail] = useState('user@example.com');
-  const [theme, setTheme] = useState('dark');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("Иван Иванов");
+  const [email, setEmail] = useState("user@example.com");
+  const [password, setPassword] = useState("");
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const token = authService.getAccessToken();
+      const payload = token ? jwtDecode(token) : null;
+      const userId = payload?.user_id;
+      const userData = await userService.getUserById(userId);
+      setName(userData.username);
+      setEmail(userData.email);
+      setTheme(userData.password);
+    };
+    fetchUserData();
+  });
   //const [notifications, setNotifications] = useState(true);
 
   const handleSave = (e) => {
     e.preventDefault();
-    alert('Настройки сохранены!');
+    alert("Настройки сохранены!");
     // Здесь можно реализовать отправку данных на сервер
-
   };
 
   return (
@@ -28,7 +42,7 @@ function SettingsPage() {
             <input
               type="text"
               value={name}
-              onChange={e => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
             />
           </label>
           <label>
@@ -36,19 +50,20 @@ function SettingsPage() {
             <input
               type="email"
               value={email}
-              onChange={e => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </label>
           <label>
             Пароль:
-            <input type="password" 
+            <input
+              type="password"
               value={password}
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </label>
           <label>
             Тема:
-            <select value={theme} onChange={e => setTheme(e.target.value)}>
+            <select value={theme} onChange={(e) => setTheme(e.target.value)}>
               <option value="dark">Тёмная</option>
               <option value="light">Светлая</option>
             </select>
@@ -61,7 +76,9 @@ function SettingsPage() {
             />
             Получать уведомления
           </label> */}
-          <button className="settings-save-btn" type="submit">Сохранить</button>
+          <button className="settings-save-btn" type="submit">
+            Сохранить
+          </button>
         </form>
       </div>
     </div>
